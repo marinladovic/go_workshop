643332743207b29d9fddaa29b656a9ba73c6bfa7
mladovic <mladovic.dev@gmail.com>
2023-10-29 08:14:15 +0100

    clear files

--- fileutils/writer.go
+++
@@ -1,7 +0,0 @@
-package fileutils
-
-import "os"
-
-func WriteToFile(filename string, content string) error {
-	return os.WriteFile(filename, []byte(content), 0644)
-}
--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"marinladovic.com/go/workshop/fileutils"
-)
-
 func main() {
-	rootPath, _ := os.Getwd()
-	filePath := rootPath + "/files/test.txt"
 
-	c, err := fileutils.ReadTextFile(filePath)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-	} else {
-		fmt.Println(c)
-
-		newContent := fmt.Sprintf("Original: %v\nDouble the Original: %v", c, c+c)
-		fileutils.WriteToFile(filePath+".output.txt", newContent)
-	}
 }
